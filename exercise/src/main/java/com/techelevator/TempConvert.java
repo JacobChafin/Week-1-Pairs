e6c4875a7a1ea2d7f7749f9aa9f650b8ccd82b10
Jacob Chafin <jacobwchafin@gmail.com>
2022-01-21 15:17:48 -0500

    Rough draft of TempCode

--- exercise/src/main/java/com/techelevator/TempConvert.java
+++ exercise/src/main/java/com/techelevator/TempConvert.java
@@ -1,9 +1,42 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class TempConvert {
 
 	public static void main(String[] args) {
 
+		String celsiusFahrenheit;
+		String temperature;
+		double actualTemperature;
+		boolean actualCelsiusFahrenheit;
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Please enter the temperature: ");
+		temperature = scanner.nextLine();
+		actualTemperature = Double.parseDouble(temperature);
+
+		do {
+			System.out.print("Please enter temperature in (C)elsius, or (F)ahrenheit? ");
+			celsiusFahrenheit = scanner.nextLine();
+//			actualCelsiusFahrenheit = Boolean.parseBoolean(celsiusFahrenheit);
+		} while ((celsiusFahrenheit == "C") || (celsiusFahrenheit == "F"));
+
+		if (celsiusFahrenheit == "F") {
+			actualTemperature = ((actualTemperature - 32) / 1.8);
+		} else {
+			actualTemperature = ((actualTemperature * 1.8) + 32);
+		}
+
+
+
+
+
+
+
+//		System.out.print(celsiusFahrenheit);
+
+		System.out.print(actualTemperature);
 	}
 
 }
