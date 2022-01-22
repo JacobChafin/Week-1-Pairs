b462038e013f3765f01f4f5dcc7eac40ab188605
Lakieta Gatson <lakietaegatson@gmail.com>
2022-01-22 18:59:51 -0500

    Fib start

--- exercise/src/main/java/com/techelevator/Fibonacci.java
+++ exercise/src/main/java/com/techelevator/Fibonacci.java
@@ -1,8 +1,16 @@
 package com.techelevator;
+import java.util.Scanner;
+
 
 public class Fibonacci {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+
+
+		System.out.print("Please enter a number: ");
+
 
 	}
 
