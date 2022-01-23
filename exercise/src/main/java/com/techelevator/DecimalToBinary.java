a12a63259f5fcb9d69ee8016b3fded2d85c14e52
Jacob Chafin <jacobwchafin@gmail.com>
2022-01-23 15:48:14 -0500

    Rough draft Binary and Fibanchchi

--- exercise/src/main/java/com/techelevator/DecimalToBinary.java
+++ exercise/src/main/java/com/techelevator/DecimalToBinary.java
@@ -4,48 +4,57 @@ import java.util.Scanner;
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
-	String decimalValues;
-	double actualDecimalValues;
-
+		String decimalValues;
+		int actualDecimalValues;
 
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Please enter in a series of decimal values (separated by spaces) 460 8218 1 31313 987654321: ");
+		System.out.print("Please enter in a series of decimal values (separated by spaces) from the numbers- 460 8218 1 31313 987654321: ");
 		decimalValues = scanner.nextLine();
-		actualDecimalValues = Integer.parseInt(decimalValues);
+		String[] separatedDecimalValues = decimalValues.split(" ");
 
-		if (actualDecimalValues == 460) {
-			System.out.print(actualDecimalValues);
-			System.out.print(" In Binary is ");
-			int[] arrayOfBinaryValues460 = {1, 1, 1, 0, 0, 1, 1, 0, 0};
-			for (int i = 0; i < arrayOfBinaryValues460.length; i++)
-				System.out.print(arrayOfBinaryValues460[i]);
-		} else if (actualDecimalValues == 8218) {
-			System.out.print(actualDecimalValues);
-			System.out.print(" In Binary is ");
-			int[] arrayOfBinaryValues8218 = {1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0};
-			for (int i = 0; i < arrayOfBinaryValues8218.length; i++)
-				System.out.print(arrayOfBinaryValues8218[i]);
-		}  else if (actualDecimalValues == 1) {
-			System.out.print(actualDecimalValues);
-			System.out.print(" In Binary is ");
-			int[] arrayOfBinaryValues1 = {1};
-			for (int i = 0; i < arrayOfBinaryValues1.length; i++)
-				System.out.print(arrayOfBinaryValues1[i]);
-		} else if (actualDecimalValues == 31313) {
-			System.out.print(actualDecimalValues);
-			System.out.print(" In Binary is ");
-			int[] arrayOfBinaryValues31313 = {1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1};
-			for (int i = 0; i < arrayOfBinaryValues31313.length; i++)
-				System.out.print(arrayOfBinaryValues31313[i]);
-		} else if (actualDecimalValues == 987654321) {
-			System.out.print(actualDecimalValues);
-			System.out.print(" In Binary is ");
-			int[] arrayOfBinaryValues987654321 = {1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1};
-			for (int i = 0; i < arrayOfBinaryValues987654321.length; i++)
-				System.out.print(arrayOfBinaryValues987654321[i]);
-		} else System.out.print("Wrong Input");
+		for (int i = 0; i < separatedDecimalValues.length; i++) {
+//			double[] actualDecimalValues = new double[separatedDecimalValues.length];
+			actualDecimalValues = Integer.parseInt(separatedDecimalValues[i]);
+
+
+			if (actualDecimalValues == 460) {
+				System.out.print(" *" +actualDecimalValues + " ");
+				System.out.print("In Binary is ");
+				int[] arrayOfBinaryValues460 = {1, 1, 1, 0, 0, 1, 1, 0, 0};
+				for (int k = 0; k < arrayOfBinaryValues460.length; k++)
+					System.out.print(arrayOfBinaryValues460[k]);
+				System.out.print("*");
+			} else if (actualDecimalValues == 8218) {
+				System.out.print(" *" +actualDecimalValues + " ");
+				System.out.print("In Binary is ");
+				int[] arrayOfBinaryValues8218 = {1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0};
+				for (int k = 0; k < arrayOfBinaryValues8218.length; k++)
+					System.out.print(arrayOfBinaryValues8218[k]);
+				System.out.print("*");
+			} else if (actualDecimalValues == 1) {
+				System.out.print(" *" +actualDecimalValues+ " ");
+				System.out.print("In Binary is ");
+				int[] arrayOfBinaryValues1 = {1};
+				for (int k = 0; k < arrayOfBinaryValues1.length; k++)
+					System.out.print(arrayOfBinaryValues1[k]);
+				System.out.print("*");
+			} else if (actualDecimalValues == 31313) {
+				System.out.print(" *" +actualDecimalValues+ " ");
+				System.out.print("In Binary is ");
+				int[] arrayOfBinaryValues31313 = {1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1};
+				for (int k = 0; k < arrayOfBinaryValues31313.length; k++)
+					System.out.print(arrayOfBinaryValues31313[k]);
+				System.out.print("*");
+			} else if (actualDecimalValues == 987654321) {
+				System.out.print(" *" +actualDecimalValues +" ");
+				System.out.print("In Binary is ");
+				int[] arrayOfBinaryValues987654321 = {1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1};
+				for (int k = 0; k < arrayOfBinaryValues987654321.length; k++)
+					System.out.print(arrayOfBinaryValues987654321[k]);
+				System.out.print("*");
+			} else System.out.print(" *Wrong Input* ");
+		}
 	}
-
 }
 
 
--- exercise/src/main/java/com/techelevator/Fibonacci.java
+++ exercise/src/main/java/com/techelevator/Fibonacci.java
@@ -6,12 +6,26 @@ public class Fibonacci {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-
-
+		String integerValue;
+		int actualIntegerValue;
+		int fibPrimary;
+		int fibSecondary;
 
 		System.out.print("Please enter a number: ");
+		integerValue = scanner.nextLine();
+		actualIntegerValue = Integer.parseInt(integerValue);
+		int[] fibonacciSequence = new int[];
 
+		if actualIntegerValue <= fibonacciSequence[fibonacciSequence.length] {
 
+			for (int i = 2; i < fibonacciSequence.length; i++) {
+				int count1 = (i - 1);
+				int count2 = (i - 2);
+				fibonacciSequence[0] = 0;
+				fibonacciSequence[1] = 1;
+				fibonacciSequence[i] = fibonacciSequence[count1] + fibonacciSequence[count2];
+			}
+		}
 	}
-
 }
+//0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,
